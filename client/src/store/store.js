45470002d84ea003e3a89e2e5db2276dfb783c8d
unknown <ellisorane@yahoo.com>
2022-09-11 15:41:47 -0400

    Another update

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import counterReducer from '../features/counter/counterSlice';
 import paymentSlice from "../features/payment/paymentSlice";
 import projectsSlice from "../features/projects/projectsSlice";
 import searchBarSlice from "../features/searchBar/searchBarSlice";
@@ -8,7 +7,6 @@ import searchBarSlice from "../features/searchBar/searchBarSlice";
 export default configureStore({
     reducer: {
         payment: paymentSlice,
-        counter: counterReducer,
         projects: projectsSlice,
         searchBar: searchBarSlice
     }
