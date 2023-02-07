adda31ee0e31fa4cee4493d11625ba0e71368d0d
unknown <ellisorane@yahoo.com>
2023-02-07 00:39:54 -0500

    update edit profile form - incomplete

--- client/src/components/Profile/EditForm/EditForm.js
+++ client/src/components/Profile/EditForm/EditForm.js
@@ -1,37 +1,76 @@
 import React from 'react'
+import axios from 'axios'
 
 import classes from './EditForm.module.scss'
 
 function FormTemplate( props ) {
-  const [ showVerifyDelete, setShowVerifyDelete ] = React.useState( false )
-  const openCloseDeleteVerfication = ( e ) => {
+    const [ activeField, setActiveField ] = React.useState( null )
+    const [ showVerifyDelete, setShowVerifyDelete ] = React.useState( false )
+    const [ formData, setFormData ] = React.useState( {
+        name: '',
+        institution: '',
+        password: '',
+        newPassword: '',
+        confirmPassword: ''
+    } )
+
+    const openCloseDeleteVerfication = ( e ) => {
         e.preventDefault()
         setShowVerifyDelete( !showVerifyDelete )
     }  
 
+    const editField = ( e, fieldName ) => {
+        e.preventDefault()
+        setActiveField( fieldName )
+    } 
+
+
+    const onChangeFormData = ( e ) => setFormData({ ...formData, [ e.target.name ]: e.target.value });
+
+
   return (
       <form className={ `${ classes.formTemplate } ${ props.showForm ? classes.showForm : undefined }` }>
         <h3 style={{ textAlign: 'center' }}>Edit Profile</h3>
+
         <label htmlFor="name">Name:</label><br />
-        <div className={ classes.formGroup }>
-            <input type="text" id="" name="name" value={ 'jSmith' } disabled /><button className={ classes.formBtn }>Change</button><br />
-        </div>
+        { activeField === 'name' ? 
+            <div className={ classes.formGroup }>
+                <input type="text" id="" name="name" value={ formData.name } onChange={ ( e ) => onChangeFormData( e ) } /><br /> 
+            </div> :
+            <div className={ classes.formGroup }>
+                <input type="text" id="" name="name" value={ formData.name } disabled /><button className={ classes.formBtn } onClick={ (e) => editField( e, 'name' ) }>Change</button><br /> 
+            </div>
+        }
+
         <label htmlFor="institution">Institution:</label><br />
-        <div className={ classes.formGroup }>
-            <input type="text" id="" name="institution" value={ 'Test Institute' } disabled /><button className={ classes.formBtn }>Change</button><br />
-        </div>
+        { activeField === 'institution' ? 
+            <div className={ classes.formGroup }>
+                <input type="text" id="" name="institution" value={ formData.institution }  onChange={ ( e ) => onChangeFormData( e ) } />
+            </div> :
+            <div className={ classes.formGroup }>
+                <input type="text" id="" name="institution" value={ formData.institution } disabled /><button className={ classes.formBtn } onClick={ (e) => editField( e, 'institution' ) }>Change</button><br /> 
+            </div>
+        }
+
         <label htmlFor="password">Password:</label><br />
-        <div className={ classes.formGroup }>
-            <input type="password" id="" name="password" value={ '123456' } disabled /><button className={ classes.formBtn }>Change</button><br />
-        </div>
+        { activeField === 'password' ? 
+            <div className={ classes.formGroup }>
+                <input type="password" id="" name="password" value={ formData.password }  onChange={ ( e ) => onChangeFormData( e ) } />
+            </div> :
+            <div className={ classes.formGroup }>
+                <input type="password" id="" name="password" value={ formData.password } disabled /><button className={ classes.formBtn } onClick={ (e) => editField( e, 'password' ) }>Change</button><br /> 
+            </div>
+        }
+
+
         {/* Hide confirm password field unless changing password */}
         <label htmlFor="password">New Password:</label><br />
         <div className={ classes.formGroup }>
-            <input type="confirm" id="" name="confirm-password" value={ '123456' } disabled /><br />
+            <input type="password" id="" name="newPassword" value={ '123456' } disabled /><br />
         </div>    
         <label htmlFor="password">Confirm New Password:</label><br />
         <div className={ classes.formGroup }>
-            <input type="confirm" id="" name="confirm-password" value={ '123456' } disabled /><br />
+            <input type="password" id="" name="confirmPassword" value={ '123456' } disabled /><br />
         </div>    
 
         <input type="submit" value="Submit"></input>
